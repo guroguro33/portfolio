74dff36d3634fe57f4a00c77c433e05ae7b5189a
toru <t02f447f@gmail.com>
2020-04-27 10:10:20 +0900

    ADD Ajax and fadeIn of skill-card
    ヒーロービューのAjaxとskillカードのフェードインを追加

--- js/script.js
+++ js/script.js
@@ -15,4 +15,44 @@ $(function(){
     $toggle_menu.removeClass('active');
     $menu.removeClass('active');
   });
+
+  // SKILLのカードがフェードイン
+  $(window).scroll(function(){ //スクロールした時、onメソッド不可
+    $('.js-fadeIn').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+      if(scroll > elemPos -windowHeight + 200){
+        $(this).addClass('fadeIn');
+      }
+
+    });
+  });
+
+  // （Ajaxの練習）ヒーロービューのタイトルクリックで記述変更
+  $('.js-change-word').on('click', function(e){
+    // e.preventDefault();
+
+    $.ajax({
+      type: 'post',
+      url: './js/ajax.php',
+      dataType: 'json',
+      data: {
+        name: 'Welcome!',
+        msg: 'MyPortfolio！'
+      }
+    }).then(function(data){
+      console.log('Ajax_success!');
+      $('.js-get-name').text(data['name']);
+      $('.js-get-title').text(data['msg']);
+    }).fail(function(jqXHR, textStatus, errorThrown, data){
+      console.log("ajax通信に失敗しました");
+      console.log("jqXHR          : " + jqXHR.status); // HTTPステータスが取得
+      console.log("textStatus     : " + textStatus);    // タイムアウト、パースエラー
+      console.log("errorThrown    : " + errorThrown.message); // 例外情報
+    });
+
+
+  });
+
 });
